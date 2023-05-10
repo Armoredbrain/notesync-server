b56b956ead24e19f5b770cfc793cdc55472d9151
tardigrade <benjamin.jaud@neomanis.io>
2023-05-10 19:56:12 +0200

    :rocket: initial setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { io } from "./ioServer";
+
+(async () => {
+  try {
+    io.listen(7777);
+    console.log("Websocket server is up");
+  } catch (error) {
+    console.log(error);
+  }
+})();
---
+++ src/ioServer.ts
@@ -0,0 +1,28 @@
+import { Server, Socket } from "socket.io";
+
+export const io = new Server();
+
+io.on("connection", (socket: Socket) => {
+  socket.on("note-update", async (data: string, callback) => {
+    console.log(data);
+    try {
+      if (typeof callback !== "function") {
+        return socket.emit("callback-error", "Hu ho, error during note-update");
+      }
+      const recipients: string[] = [];
+      for (const specificSocket of io.sockets.sockets.values()) {
+        if (socket.id !== specificSocket.id) {
+          recipients.push(specificSocket.id);
+        }
+      }
+      io.to(recipients).emit("incoming-data", data);
+      callback("Ok");
+    } catch (error) {
+      callback("Error");
+    }
+  });
+  socket.on("disconnect", async (reason) => {
+    // this one trigger when user close the app
+    console.log(`user left : ${reason}`);
+  });
+});
